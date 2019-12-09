2daf899932ca6eebb3ab437370bacd03c4a12891
Kevin Avrillon <kevin@avrillon.me>
2019-12-09 21:23:41 +0100

    test linking

--- src/test.js
+++ src/test.js
@@ -6,18 +6,29 @@ const getResponse = myxml => ({
   body: myxml
 });
 
-exports.handler = async (event, context, callback) => {
-  console.log('TCL: exports.handler -> event.headers', event.headers);
-  console.log('TCL: exports.handler -> context', context);
+const getClient = async url => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        url: url
+      });
+    }, 1000);
+  });
+};
 
-  const response = getResponse(`<?xml version="1.0" encoding="UTF-8"?>
+exports.handler = async event => {
+  const from = event.headers['x-from'];
+  console.log('TCL: FROM: ', from);
+  const client = await getClient(from);
+
+  return getResponse(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://www.example.com</loc>
+        <loc>${client.url}</loc>
         <lastmod>2005-01-01</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
   </urlset>`);
-  callback(null, response);
 };
