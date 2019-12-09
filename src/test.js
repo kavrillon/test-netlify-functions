6208416f595dbb8cb30fba3bf01d86aff89f2169
Kevin Avrillon <kevin@avrillon.me>
2019-12-09 21:41:18 +0100

    show vars

--- src/test.js
+++ src/test.js
@@ -11,18 +11,19 @@ const getData = async from => {
 
 exports.handler = async event => {
   const from = event.headers['x-from'];
-  console.log('TCL: from', from);
   const upperfrom = event.headers['X-From'];
-  console.log('TCL: upperfrom', upperfrom);
   const custom = event.headers['x-custom-token'];
-  console.log('TCL: custom', custom);
   const uppercustom = event.headers['X-Custom-Token'];
-  console.log('TCL: uppercustom', uppercustom);
 
   const result = await getData(from);
 
   return {
     statusCode: 200,
-    body: 'Requested Slug: ' + result.slug
+    body: `<ul>
+      <li>from: ${from}</li>
+      <li>upperfrom: ${upperfrom}</li>
+      <li>custom: ${custom}</li>
+      <li>: ${uppercustom}</li>
+    </ul>`
   };
 };
