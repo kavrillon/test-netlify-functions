f17d5530245e43f15634197d7d5ec0ae1d462a97
Kevin Avrillon <kevin@avrillon.me>
2019-12-10 15:28:45 +0100

    netlify hostname

--- src/from.js
+++ src/from.js
@@ -1,36 +1,16 @@
-const getData = async from => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve({
-        id: 1,
-        slug: from
-      });
-    }, 1000);
-  });
-};
-
 exports.handler = async event => {
   console.log(event.headers);
-  const referer = event.headers['referer'];
-  const from = event.headers['x-from'];
-  const upperfrom = event.headers['X-From'];
-  const custom = event.headers['x-custom-token'];
-  const uppercustom = event.headers['X-Custom-Token'];
-
-  const result = await getData(from);
-
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/html'
     },
     body: `<ul>
-      <li>Referer: ${referer}</li>
-      <li>Requested data: ${result.id}</li>
-      <li>from: ${from}</li>
-      <li>upperfrom: ${upperfrom}</li>
-      <li>custom: ${custom}</li>
-      <li>uppercustom: ${uppercustom}</li>
+      <li>Referer: ${event.headers['referer']}</li>
+      <li>x-from: ${event.headers['x-from']}</li>
+      <li>X-From: ${event.headers['X-From']}</li>
+      <li>X-Netlify-Hostname: ${event.headers['X-Netlify-Hostname']}</li>
+      <li>x-netlify-hostname: ${event.headers['x-netlify-hostname']}</li>
     </ul>`
   };
 };
